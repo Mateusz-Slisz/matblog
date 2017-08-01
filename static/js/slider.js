17156a571221c8e9faeec2a8702add0ad906e7cf
Mateusz Slisz <mat.slisz@yahoo.com>
2017-08-01 23:02:26 +0200

    slider repair

--- static/js/slider.js
+++ static/js/slider.js
@@ -12,15 +12,15 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("batonik");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-red", "");
+     dots[i].className = dots[i].className.replace("w3-dark-grey", "");
   }
   x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-red";
+  dots[slideIndex-1].className += " w3-dark-grey";
 }
