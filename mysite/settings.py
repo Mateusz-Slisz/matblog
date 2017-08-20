23d74475666717ceb3b94211046040a65dbd57f2
Mateusz Slisz <mat.slisz@yahoo.com>
2017-08-20 06:16:24 +0200

    Settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ue*w0&$vqdvalsu_wn#n-i+hsu8l2c&%+$^*s7r2lbx#$!&5mn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
